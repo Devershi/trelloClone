8d217bc8008e1b103f070df738ee1732a81b9b31
devershi2007 <devershi2007@gmail.com>
2022-10-09 12:03:13 +0200

    cardserviceTest Initial commit.

--- src/main/java/com/trello/service/CardService.java
+++ src/main/java/com/trello/service/CardService.java
@@ -1,29 +1,63 @@
 package com.trello.service;
 
 import com.trello.model.Card;
+import com.trello.model.Column;
 
 import java.util.*;
 
 public class CardService implements ICardService{
     Map<Long, Long> cardToColumnMapping;
     Map<String, Set<Long>> tagVsCardMapping;
-    Map<Long, Set<Long>> timestampCardMapping;
+    SortedMap<Long, Long> timestampCardMapping;
     Map<Long, Card> cardMap;
     Set<Long> activeCards;
     Set<Long> deletedCards;
 
     public CardService(){
-this.activeCards = new HashSet<>();
-this.deletedCards = new HashSet<>();
-this.cardMap = new HashMap<>();
-this.timestampCardMapping = new LinkedHashMap<>();
-this.cardToColumnMapping = new HashMap<>();
-this.tagVsCardMapping = new HashMap<>();
+       this.activeCards = new HashSet<>();
+       this.deletedCards = new HashSet<>();
+       this.cardMap = new HashMap<>();
+       this.timestampCardMapping = new TreeMap<>();
+       this.cardToColumnMapping = new HashMap<>();
+       this.tagVsCardMapping = new HashMap<>();
+    }
+
+    public boolean createCard(Long id, String name, String description, Column column, Set<String> labels, Long createdBy) {
+        if(id == null || name.isEmpty() || column == null){
+            return  false;
+        }
+        if(activeCards.contains(id) || deletedCards.contains(id)){
+            return false;
+        }
+        Card c = new Card(id, name, description, labels, createdBy);
+        activeCards.add(id);
+        cardMap.put(id, c);
+        cardToColumnMapping.put(id, column.getId());
+        timestampCardMapping.put(System.currentTimeMillis(), id);
+        if(labels != null) {
+            for (String label : labels) {
+                Set<Long> cardsWithTag = tagVsCardMapping.get(label);
+                if (cardsWithTag == null) {
+                    cardsWithTag = new HashSet<>();
+                }
+                cardsWithTag.add(id);
+                tagVsCardMapping.put(label, cardsWithTag);
+            }
+        }
+
+        return true;
     }
 
     @Override
     public List<Card> getAllCardsAfterTime(Long timestamp) {
-        return null;
+        if(timestamp == null)
+            return Collections.emptyList();
+        SortedMap<Long, Long> tailMap = this.timestampCardMapping.tailMap(timestamp);
+        List<Card> res = new ArrayList<>();
+        for(Long id: tailMap.values()){
+            res.add(cardMap.get(id));
+        }
+        return  res;
     }
 
     // Map<String, Set<Card>> tagVsCardIgnoreCaseMapping;
---
+++ src/test/java/CardServiceTest.java
@@ -0,0 +1,58 @@
+import com.trello.model.Card;
+import com.trello.model.Column;
+import com.trello.service.BoardService;
+import com.trello.service.CardService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class CardServiceTest {
+    static CardService cs;
+    @BeforeAll
+    public static void setup(){
+        cs = new CardService();
+    }
+
+    @Test
+    public void testCardAddWithoutId(){
+        boolean created = cs.createCard(null, "card1", "des", null, null, 1L);
+        Assertions.assertEquals(created, false);
+    }
+    @Test
+    public void testCardAddWithoutName(){
+        boolean created = cs.createCard(1L, "", "des", null, null, 1L);
+        Assertions.assertEquals(created, false);
+    }
+    @Test
+    public void testCardAddWithoutColumnName(){
+        boolean created = cs.createCard(1L, "card1", "des", null, null, 1L);
+        Assertions.assertEquals(created, false);
+    }
+
+    @Test
+    public void testCardAdd(){
+        Column c = new Column(1L,"c1","c1",1L);
+        boolean created = cs.createCard(1L, "card1", "des", c, null, 1L);
+        Assertions.assertEquals(created, true);
+    }
+
+    @Test
+    public void testCardEmptyForCurrentTime(){
+        Column c = new Column(1L,"c1","c1",1L);
+        boolean created = cs.createCard(2L, "card2", "des", c, null, 2L);
+        Assertions.assertEquals(created, true);
+        List<Card> cardsNow = cs.getAllCardsAfterTime(System.currentTimeMillis());
+        Assertions.assertEquals(cardsNow.size(), 0);
+    }
+
+    @Test
+    public void testCardForPastTime(){
+        Column c = new Column(1L,"c1","c1",1L);
+        boolean created = cs.createCard(3L, "card3", "des", c, null, 2L);
+        Assertions.assertEquals(created, true);
+        List<Card> cardsNow = cs.getAllCardsAfterTime(System.currentTimeMillis() - 2000);
+        Assertions.assertTrue(cardsNow.size() > 0);
+    }
+}
